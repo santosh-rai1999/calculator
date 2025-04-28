b1929a85f60fdbcb18eea1d2aa64a0a6ac84dc37
Santosh Nachhiring Rai <rayee.santosh123@gmail.com>
2025-04-28 12:44:35 +0545

    calculator using javascript and react

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./style.css";
+import Meals from "./Meals";
+import Calculator from "./Calculator";
+import "./style.css";
+
+const App = () => {
+  // return <Meals />;
+  return <Calculator/>
+};
+
+export default App;
